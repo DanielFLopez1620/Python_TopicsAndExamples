eeff7557fa1c3d4c93a176cd863ead5846c8391b
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2022-03-22 03:14:13 -0500

    Logic and comparatives operators

---
+++ BasicPython/P06_Logic.py
@@ -0,0 +1,8 @@
+print("You've already read about comparison operators, now we have the logic operators: ")
+bool1 = True
+bool2 = False
+print(f"Value examples: {bool1} and {bool2} for 'and', 'or' ando 'not'")
+print(f"{bool1} and {bool2}: {bool1 and bool2}")
+print(f"{bool1} or  {bool2}: {bool1 or bool2}")
+print(f"not {bool1}: {not bool1}")
+print("\nYou can also combine them in a statment, always returns a boolean result\n")
