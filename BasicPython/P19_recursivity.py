7a86a5fd58091a06f7d2bc0361f782361da025b1
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2022-04-07 03:07:06 -0500

    Fibonacci and recursivity

--- BasicPython/P19_recursivity.py
+++ BasicPython/P19_recursivity.py
@@ -11,7 +11,15 @@ def factorial(num):
     return num * factorial(num - 1)
 
 
+def fibonacci(num):
+    if num == 0 or num == 1:
+        return 1
+    return fibonacci(num - 1) + fibonacci(num - 2)
+
 print("Recursivity: A function that calls itself, let's see the factorial example: ")
 var = int(input("Type a number: "))
 print(f"Factorial of {var} is {factorial(var)}")
 print(f"You can see the limit of recursion with the command 'sys.getrecursionlimit()': {sys.getrecursionlimit()}")
+print("\nFinally, another example of recursivity is the Fibonacci series, as you can see next:")
+var = int(input("What number you would like to see with Fibonacci: "))
+print(f"Result:{fibonacci(var)}")
