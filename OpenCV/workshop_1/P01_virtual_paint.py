ae3a5fbc2e9c49658adc36ada0c294d59eea76e4
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2022-06-21 16:34:52 -0500

    P1: Virtual Paint. Original added and color fixes

--- OpenCV/workshop_1/P01_virtual_paint.py
+++ OpenCV/workshop_1/P01_virtual_paint.py
@@ -12,21 +12,22 @@ def getContour2Paint(image):
             perimeter = cv2.arcLength(cnt,True)
             corners = cv2.approxPolyDP(cnt , 0.02*perimeter, True)
             x, y, w, h = cv2.boundingRect(corners)
-    return (x+w)//2, y
-    # return image
+            # cv2.imshow("Contours", image)
+    return (x-w)//2, y
   
 
 def drawAndPaint(image, colorPoints, colorBGR):
-    for i, point in enumerate(colorPoints):
-        cv2.circle(image, (point[0], point[1]), 10, colorBGR[i], cv2.FILLED)
+    for point in colorPoints:
+        cv2.circle(image, (point[0], point[1]), 10, colorBGR[point[2]], cv2.FILLED)
     return image
 
 
 def paintColor(image, colorsHSV, colorBGR):
+    colorList = ("Orange", "Purple", "Green")
     points = [] ## [x, y, color] of each color
+    imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     for i, color in enumerate(colorsHSV):
-        imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower = np.array( color[0:3])
+        lower = np.array(color[0:3])
         upper = np.array (color[3:6])
         mask = cv2.inRange(imgHSV, lower, upper)
         # cv2.imshow(str(colorList[i]), mask)
@@ -34,22 +35,24 @@ def paintColor(image, colorsHSV, colorBGR):
         cv2.circle(image, (x,y), 10, colorBGR[i], cv2.FILLED)
         if x != 0 and y != 0:
             points.append([x ,y, i])
-    return drawAndPaint(image, points, colorBGR)
-
+    return points
 
 
 def main():
     # Definition of the colors for the program
-    colorsPaints = [
-        [5, 107, 0, 19, 255, 255], # Orange
-        [133, 56, 9, 159, 159, 255], # Purple
-        [57, 76, 0, 100, 255, 255] # Green
+    colorsPaints = [ # H_min, S_min, V_min, H_max, S_max, V_max
+            [0,143,155,26,255,255], # Orange
+            [105,76,117,141,164,207], # Purple
+            [63,113,109,103,255,255] # Green
         ]
     colorBGR = [[51, 153, 255],
         [255, 0, 255],
         [0, 255, 0]]
-    
     # colorList = ("Orange", "Purple", "Green")
+    
+    # Painted points
+    onBoard = []
+
     # Capture video of the default web cam
     video = cv2.VideoCapture(0)
 
@@ -60,21 +63,28 @@ def main():
     video.set(3, camWidth)
     video.set (10, 150)
 
-
     # Loop to run the video
     while True:
         # Convert and read sequence
         success, fotogram = video.read()
-        
+        imgPainted = fotogram.copy()
         # Identify colors
-        imgPainted = paintColor(fotogram, colorsPaints, colorBGR)
-        cv2.imshow("Video_Example", fotogram)
+        points = paintColor(imgPainted, colorsPaints, colorBGR)
+
+        # Paint the colors 
+        if len(points)!=0:
+            for newP in points:
+                onBoard.append(newP)
+        if len(onBoard)!=0:
+            imgPainted = drawAndPaint(fotogram, onBoard, colorBGR)
+        # Display video and virtual paint
         cv2.imshow("Painted_Board", imgPainted)
 
         # Option to stop the reproduction
-        if cv2.waitKey(1) and 0xFF == ord('q'):
+        if cv2.waitKey(1) and (0xFF == ord('q') or not success):
             break
     # TODO: Comment functions and verify colors
+    # TODO: Fix the x-axis orientation
 
 
 if __name__ == '__main__':
