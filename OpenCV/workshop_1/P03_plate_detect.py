19e071bd469b1c7d1e5ebf5bdd49b342b76a0f57
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2022-06-27 12:24:22 -0500

    P3: Detect number plate. Added

---
+++ OpenCV/workshop_1/P03_plate_detect.py
@@ -0,0 +1,46 @@
+import cv2
+
+def main():
+    # Parameters for camera and cascade
+    width = 640
+    height = 480
+    area = 0
+    color = (0, 0, 255)
+    numPlateCascade = cv2.CascadeClassifier("Resources/haarcascade_russian_plate_number.xml")
+
+    # Select default webcam and configure input
+    video = cv2.VideoCapture(0)
+    video.set(4, height)
+    video.set(3, width)
+    video.set (10,100)
+
+    # Loop to run the video
+    while True:
+        # Convert and read sequence
+        success, fotogram = video.read()
+        
+        
+        # Preprocess image
+        imgGray = cv2.cvtColor(fotogram, cv2.COLOR_BGR2GRAY)
+
+        # Use the cascade
+        facesDetected = numPlateCascade.detectMultiScale(imgGray, 1.1, 4)
+        
+        # Define and add bounding boxes
+        for (x,y,h,w) in facesDetected:
+            area = w*h
+            if area > 500:
+                cv2.rectangle(fotogram, (x,y), (x+w,y+h), (255,255,0), 3)
+                cv2.putText(fotogram, "Plate detected", (x,y-5), 
+                             cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
+                imgCrop = fotogram[u:y+h, x:x+w]
+                cv2.imshow("Detected", imgCrop)
+        
+        # Option to stop the reproduction
+        cv2.imshow("Detect_Plate", fotogram)
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+
+
+if __name__ == '__main__':
+    main()
